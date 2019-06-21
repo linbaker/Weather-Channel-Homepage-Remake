79e0d3801f90893f420d03697092b33c2cf88924
Lindsey Baker <lindsey.baker0@gmail.com>
2019-06-21 09:13:46 -0700

    Move API keys to environments and ignore

--- src/app/weather-data.service.ts
+++ src/app/weather-data.service.ts
@@ -5,6 +5,14 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class WeatherDataService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  searchWeatherData(cityName: string, weatherAPIKey: string): Observable<any> {
+      return this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${weatherAPIKey}`)
+          .map(response => response.json())
+          .catch(error => {
+              console.error(error);
+              return Observable.throw(error.json())
+          });
+  }
 }
---
+++ src/environments/api-keys.ts
@@ -0,0 +1,11 @@
+export const masterFirebaseConfig = {
+    apiKey: "xxxx",
+    authDomain: "xxxx.firebaseapp.com",
+    databaseURL: "https://xxxx.firebaseio.com",
+    storageBucket: "xxxx.appspot.com",
+    messagingSenderId: "xxxx"
+  };
+
+export const weatherAPI = {
+  weatherAPIKey: "1a884f65ad6bb67b17e772389e29b35e"
+}
