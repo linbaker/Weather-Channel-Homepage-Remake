515839c9930e8b2455d2882dc6ce5774f0e49268
Lindsey Baker <lindsey.baker0@gmail.com>
2019-06-24 07:39:33 -0700

    Add OnSubmit function for member signup, delete spec folder

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
+
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  state: string = '';
+  error: any;
+
+  constructor(public af: AngularFire,private router: Router) {
+
+  }
+
+  onSubmit(formData) {
+    if(formData.valid) {
+      console.log(formData.value);
+      this.af.auth.createUser({
+        email: formData.value.email,
+        password: formData.value.password
+      }).then(
+        (success) => {
+        this.router.navigate(['/members'])
+      }).catch(
+        (err) => {
+        this.error = err;
+      })
+    }
+  }
 
   ngOnInit() {
   }
