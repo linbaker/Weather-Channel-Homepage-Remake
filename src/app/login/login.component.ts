2c572e7e12413c78a69a4d63c24efcf8f494eacc
Lindsey Baker <lindsey.baker0@gmail.com>
2019-06-24 07:23:30 -0700

    Add Angular Fire and Router to login, add login google function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  error: any;
+  constructor(public af: AngularFire, private router: Router) {
+
+      this.af.auth.subscribe(auth => {
+      if(auth) {
+        this.router.navigateByUrl('/members');
+      }
+    });
+
+  }
+
+  loginGoogle() {
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    }).then(
+        (success) => {
+        this.router.navigate(['/members']);
+      }).catch(
+        (err) => {
+        this.error = err;
+      })
+  }
+
 
   ngOnInit() {
   }
